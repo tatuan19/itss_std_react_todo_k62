eadddb276d6e427d3e59c3b8672c382916d2aef8
tatuan19 <trananhtuan12a10@gmail.com>
2021-05-21 09:28:27 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAMql2W1G--uUhCYpOWiDKQdHWJGRUqE84",
+    authDomain: "fir-sample-5b7ab.firebaseapp.com",
+    projectId: "fir-sample-5b7ab",
+    storageBucket: "fir-sample-5b7ab.appspot.com",
+    messagingSenderId: "735359494978",
+    appId: "1:735359494978:web:513ec21afcfaf3f88bb6db",
+    measurementId: "G-GXZY0L446B"
+};
+
+firebase.initializeApp(firebaseConfig);
